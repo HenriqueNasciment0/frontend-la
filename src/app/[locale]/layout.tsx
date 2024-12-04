7b81216c69b4a49221ad45d9227f027128b0ce73
Henrique Nasciment0 <hsncorretor@gmail.com>
2024-12-04 16:55:05 -0300

    fix: promisse of params

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,9 +9,9 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
-  const { locale } = params;
+  const { locale } = await params;
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   if (!routing.locales.includes(locale as any)) {
